059b6d941f2582dcfa121f8193152944ca512a3c
Sven Lindauer <slindauer@inovex.de>
2019-10-02 14:18:11 +0200

    Fixed hero service test

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HeroService } from './hero.service';
+
+describe('HeroService', () => {
+  let heroService: HeroService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    heroService = TestBed.get(HeroService);
+  });
+
+  it('should be created', () => {
+    expect(heroService).toBeTruthy();
+  });
+});
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.less']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  add(name: string) {
+    name = name.trim();
+    if(!name) {return};
+    this.heroService.addHero({name} as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(id: number){
+    this.heroes = this.heroes.filter(heroe => heroe.id !== id)
+    this.heroService.deleteHero(id)
+    .subscribe();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+}
