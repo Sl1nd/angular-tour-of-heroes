a356b645f0b843c06ad77cc90135913375721021
Sven Lindauer <slindauer@inovex.de>
2019-10-04 10:44:16 +0200

    fix dashboard test

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,16 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+import { of } from 'rxjs';
 import { DashboardComponent } from './dashboard.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
+  let heroServiceSpy: jasmine.SpyObj<HeroService>;
+
   beforeEach(async(() => {
+    const testData: Hero[] = [{
+      id: 1,
+      name: 'Achilis'
+    }, {
+        id: 2,
+        name: 'Hextor'
+      }];
+
+    // Create a fake HeroService object with a `getHeroes()` spy
+    const spy = jasmine.createSpyObj('HeroService', ['getHeroes']);
+    // Make the spy return a synchronous Observable with the test data
+    const getHeroesSpy = spy.getHeroes.and.returnValue(of(testData));
+
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [
+        DashboardComponent
+       ],
+      providers: [{ provide: HeroService, useValue: spy }],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
+
+    heroServiceSpy = TestBed.get(HeroService);
   }));
 
   beforeEach(() => {
