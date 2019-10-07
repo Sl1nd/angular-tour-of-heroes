5435d9e3acefc7ebeae839d93fec5f8c30390561
Sven Lindauer <slindauer@inovex.de>
2019-10-07 11:55:18 +0200

    Added detail tests

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -2,23 +2,36 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import {HeroService} from '../hero.service';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from '../hero';
+import { of } from 'rxjs';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let heroServiceSpy: jasmine.SpyObj<HeroService>;
+  let locationSpy: jasmine.SpyObj<Location>;
+  let testHero; Hero;
 
   beforeEach(async(() => {
-    const locationSpy = jasmine.createSpyObj('Location', ['back']);
+    testHero = {
+      id: 1, name: 'Busta Rhymes'
+    };
+    locationSpy = jasmine.createSpyObj('Location', ['back']);
+    heroServiceSpy = jasmine.createSpyObj('HeroService', ['getHero', 'updateHero']);
+
+    heroServiceSpy.getHero.and.returnValue(of(testHero));
+    heroServiceSpy.updateHero.and.returnValue(of(testHero));
 
     TestBed.configureTestingModule({
       declarations: [ HeroDetailComponent ],
       providers: [{
          provide: ActivatedRoute, useValue: {snapshot: {paramMap: {get: () => 1}}}
          },{ provide: Location, useValue: locationSpy
-        }],
+        }, { provide: HeroService, useValue: heroServiceSpy}],
       imports: [FormsModule, HttpClientTestingModule]
     })
     .compileComponents();
@@ -30,7 +43,44 @@ describe('HeroDetailComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render hero title', () => {
+    const heroDetail: HTMLElement = fixture.nativeElement;
+    expect(heroDetail.querySelector('h2').textContent).toContain('BUSTA RHYMES Details');
+  });
+
+  it('should render save button', () => {
+    const heroDetail: HTMLElement = fixture.nativeElement;
+
+    expect(heroDetail.querySelectorAll('button')[0].textContent).toEqual('Save');
+  });
+
+  it('should updatehero on clicking save button', () => {
+    const heroDetail: HTMLElement = fixture.nativeElement;
+    const saveButton = heroDetail.querySelectorAll('button')[0];
+    saveButton.click();
+    expect(heroServiceSpy.updateHero).toHaveBeenCalledWith(testHero);
+  });
+
+  it('should navigate back on updating Hero', () => {
+    spyOn(component, 'goBack');
+    const heroDetail: HTMLElement = fixture.nativeElement;
+    const saveButton = heroDetail.querySelectorAll('button')[0];
+    saveButton.click();
+
+    expect(component.goBack).toHaveBeenCalled();
+  });
+
+  it('should call goBack on clicking back Button', () => {
+    spyOn(component, 'goBack');
+    const heroDetail: HTMLElement = fixture.nativeElement;
+    const saveButton = heroDetail.querySelectorAll('button')[1];
+    saveButton.click();
+
+    expect(component.goBack).toHaveBeenCalled();
+  });
+
+  it('should render back button', () => {
+    const heroDetail = fixture.nativeElement;
+    expect(heroDetail.querySelectorAll('button')[1].textContent).toEqual('go back');
   });
 });
