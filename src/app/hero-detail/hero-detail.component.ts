5007cd83e5504762b843d72b79ae1485c69e099f
Sven Lindauer <slindauer@inovex.de>
2019-10-08 11:04:40 +0200

    Added search tests

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -38,3 +38,5 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 }
+
+
--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,29 +1,63 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HeroSearchComponent } from './hero-search.component';
+import { Hero } from '../hero';
+import {newEvent, RouterLinkDirectiveStub} from '../../testing';
+import { of } from 'rxjs';
+import { HeroService } from '../hero.service';
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
   let fixture: ComponentFixture<HeroSearchComponent>;
+  let testHeroes: Hero[];
+  let heroServiceSpy: jasmine.SpyObj<HeroService>;
+  let heroSearchComponent: HTMLElement;
+  let inputField: HTMLInputElement;
 
   beforeEach(async(() => {
+    testHeroes = [{
+      id: 1, name: 'Busta Rhymes'
+    }];
+
+    heroServiceSpy = jasmine.createSpyObj('HeroService', ['searchHeroes']);
+    heroServiceSpy.searchHeroes.and.returnValue(of(testHeroes));
+
     TestBed.configureTestingModule({
-      declarations: [ HeroSearchComponent ],
-      imports: [HttpClientTestingModule],
-      schemas: [NO_ERRORS_SCHEMA]
+      declarations: [HeroSearchComponent, RouterLinkDirectiveStub ],
+      providers: [{ provide: HeroService, useValue: heroServiceSpy }],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
-  }));
+    }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeroSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      fixture = TestBed.createComponent(HeroSearchComponent);
+      component = fixture.componentInstance;
+      heroSearchComponent = fixture.nativeElement;
+      inputField = heroSearchComponent.querySelector('input');
+      fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  afterEach(() => {
+      inputField.value = '';
+    });
+
+  it('Should search term', fakeAsync(() => {
+    const spy = spyOn(component, 'search');
+    inputField.value = 'foo';
+    inputField.dispatchEvent(newEvent('input'));
+    tick(300);
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith('foo');
+  }));
+
+
+  it('Should call hero service', fakeAsync(() => {
+    inputField.value = 'bar';
+    inputField.dispatchEvent(newEvent('input'));
+    tick(300);
+    fixture.detectChanges();
+    expect(heroServiceSpy.searchHeroes).toHaveBeenCalledWith('bar');
+  }));
 });
