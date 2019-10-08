3e4be4ce1db02dbe8f15836d0adc70c69d0e0737
Sven Lindauer <slindauer@inovex.de>
2019-10-08 11:05:07 +0200

    Added test utilities

---
+++ src/testing/index.ts
@@ -0,0 +1,7 @@
+import { RouterLinkDirectiveStub } from './router-link-directive-stub';
+import {newEvent} from './newEvent';
+
+export {
+  RouterLinkDirectiveStub,
+  newEvent
+}
---
+++ src/testing/router-link-directive-stub.ts
@@ -0,0 +1,16 @@
+import { Directive, Input, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[routerLink]'
+})
+class RouterLinkDirectiveStub {
+  @Input('routerLink') linkParams: any;
+  navigatedTo: any = null;
+
+  @HostListener('click')
+  onClick() {
+    this.navigatedTo = this.linkParams;
+  }
+}
+
+export {RouterLinkDirectiveStub};
